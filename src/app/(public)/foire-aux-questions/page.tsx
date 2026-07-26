fd9f7269cb4ed78de2b2a0ce0cbf00cdb5ca3ce9
ibraum <ibraumodnokpro@gmail.com>
2026-07-26 12:38:18 +0000

    FOIRES AUX QUESTIONS FINNISH

--- src/app/(public)/foire-aux-questions/page.tsx
+++ src/app/(public)/foire-aux-questions/page.tsx
@@ -1,11 +1,13 @@
 import CTA from '@/components/ui/organisms/CTA'
 import FAQ from '@/components/ui/organisms/faq/FAQ'
+import Hero from '@/components/ui/organisms/faq/Hero'
 import WhyChooseUs from '@/components/ui/organisms/faq/WhyChooseUs'
 import { faqCtaSection } from '@/constants/homeSectionsData'
 
 const page = () => {
     return (
         <>
+            <Hero />
             <WhyChooseUs />
             <FAQ />
             <CTA {...faqCtaSection} />
