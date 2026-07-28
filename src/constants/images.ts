ed764d1c225ae55fb3ac8fb8d4a40c95703c65a7
ibraum <ibraumodnokpro@gmail.com>
2026-07-28 12:15:55 +0000

    Create images.ts file to use nextjs static functionnalmity

---
+++ src/constants/images.ts
@@ -0,0 +1,199 @@
+import Discover from "@/public/images/discover.png";
+import Resources from "@/public/images/resources.png";
+import Verify from "@/public/images/verify.png";
+import Commit from "@/public/images/commit/commit.png";
+import Commit2 from "@/public/images/commit/commit-2.png";
+import CurveU from "@/public/images/curve-u.png";
+import Curve from "@/public/images/curve.png";
+import CurveCyan from "@/public/images/curve-cyan.png";
+import Goat from "@/public/images/goat.png";
+import Student from "@/public/images/student.png";
+import Teacher from "@/public/images/teacher.png";
+import Lion from "@/public/images/lion.png";
+import ContactUs from "@/public/images/contact-us.png";
+import WhyChooseUsIllustration1 from "@/public/images/why-choose-us/illustration-1.png";
+import WhyChooseUsIllustration2 from "@/public/images/why-choose-us/illustration-2.png";
+import Info from "@/public/images/info.png";
+import Hero from "@/public/images/hero.png";
+import HowItWork from "@/public/images/howItWork.png";
+import Davys from "@/public/images/davys.png";
+import Fnepi from "@/public/images/fnepi.png";
+import HomeFly from "@/public/images/home-fly.png";
+import Users from "@/public/images/users.png";
+import TeacherIllustration from "@/public/images/teacherIllustration.png";
+import WhatFamilySay from "@/public/images/whaFamilySay.png";
+import WhatHelearn from "@/public/images/whatHelearn.png";
+import SeeEverything from "@/public/images/seeEverything.png";
+import TrackAll from "@/public/images/trackAll.png";
+import HowItWorkHero from "@/public/images/how-it-work/hero.png";
+import HowItWorkIllustration from "@/public/images/how-it-work/illustration.png";
+import HowItWorkOne from "@/public/images/how-it-work/one.png";
+import HowItWorkTwo from "@/public/images/how-it-work/two.png";
+import HowItWorkThree from "@/public/images/how-it-work/three.png";
+import HowItWorkFour from "@/public/images/how-it-work/four.png";
+import HowItWorkIllustration1 from "@/public/images/how-it-work/illustration-1.png";
+import HowItWorkIllustration2 from "@/public/images/how-it-work/illustration-2.png";
+import HowItWorkIllustration3 from "@/public/images/how-it-work/illustration-3.png";
+import HowItWorkIllustration4 from "@/public/images/how-it-work/illustration-4.png";
+import HowItWorkIllustration5 from "@/public/images/how-it-work/illustration-5.png";
+import HowItWorkIllustration6 from "@/public/images/how-it-work/illustration-6.png";
+import HowItWorkIllustration7 from "@/public/images/how-it-work/illustration-7.png";
+import HowItWorkIllustration8 from "@/public/images/how-it-work/illustration-8.png";
+import SelectionStepIllustration1 from "@/public/images/selection-step/illustration-1.png";
+import SelectionStepIllustration2 from "@/public/images/selection-step/illustration-2.png";
+import SelectionStepIllustration3 from "@/public/images/selection-step/illustration-3.png";
+import SelectionStepIllustration4 from "@/public/images/selection-step/illustration-4.png";
+import SelectionStepIllustration5 from "@/public/images/selection-step/illustration-5.png";
+import SelectionStepIllustration6 from "@/public/images/selection-step/illustration-6.png";
+import SelectionStepIllustration7 from "@/public/images/selection-step/illustration-7.png";
+import SelectionStepIllustration8 from "@/public/images/selection-step/illustration-8.png";
+import TeacherHorizScroll from "@/public/images/TeacherHorizScroll.png";
+import Teacher1 from "@/public/images/teachers/teacher-1.png";
+import Teacher2 from "@/public/images/teachers/teacher-2.png";
+import Teacher3 from "@/public/images/teachers/teacher-3.png";
+import Teacher4 from "@/public/images/teachers/teacher-4.png";
+import Teacher5 from "@/public/images/teachers/teacher-5.png";
+import Teacher6 from "@/public/images/teachers/teacher-6.png";
+import Teacher7 from "@/public/images/teachers/teacher-7.png";
+import Teacher8 from "@/public/images/teachers/teacher-8.png";
+import Avatar1 from "@/public/images/testimonies/avatar-1.svg";
+import Avatar2 from "@/public/images/testimonies/avatar-2.svg";
+import Avatar3 from "@/public/images/testimonies/avatar-3.svg";
+import Avatar4 from "@/public/images/testimonies/avatar-4.svg";
+import Avatar5 from "@/public/images/testimonies/avatar-5.svg";
+import Avatar6 from "@/public/images/testimonies/avatar-6.svg";
+import Avatar7 from "@/public/images/testimonies/avatar-7.svg";
+import Cta1 from "@/public/images/cta/cta-1.png";
+import Cta2 from "@/public/images/cta/cta-2.png";
+import Cta3 from "@/public/images/cta/cta-3.png";
+import Cta4 from "@/public/images/cta/cta-4.png";
+import Value1 from "@/public/images/value-1.png";
+import Value2 from "@/public/images/value-2.png";
+import Value3 from "@/public/images/value-3.png";
+import ChallengesLearning from "@/public/images/challenges/Learning.png";
+import ChallengesBrain from "@/public/images/challenges/Brain.png";
+import ChallengesBag from "@/public/images/challenges/Bag.png";
+import ChallengesCup from "@/public/images/challenges/Cup.png";
+import ChallengesHug from "@/public/images/challenges/Hug.png";
+import ChallengesDirection from "@/public/images/challenges/Direction.png";
+import ValuesExcellence from "@/public/images/values/Excellence.png";
+import ValuesProfessionnalisme from "@/public/images/values/Professionnalisme.png";
+import ValuesInnovation from "@/public/images/values/Innovation.png";
+import ValuesIntegrity from "@/public/images/values/Integrity.png";
+import ValuesEngagement from "@/public/images/values/Engagement.png";
+import ValuesRespect from "@/public/images/values/Respect.png";
+import Math from "@/public/images/subjects/Math.svg";
+import PH from "@/public/images/subjects/PH.svg";
+import SVT from "@/public/images/subjects/SVT.svg";
+import English from "@/public/images/subjects/English.svg";
+import French from "@/public/images/subjects/French.svg";
+import Spanish from "@/public/images/subjects/Spanish.svg";
+import HG from "@/public/images/subjects/HG.svg";
+import Philosophy from "@/public/images/subjects/Philosophy.svg";
+import Arabic from "@/public/images/subjects/Arabic.svg";
+import Computer from "@/public/images/subjects/Computer.svg";
+import Pedagogie from "@/public/images/team/pedagogie.png";
+import Supervision from "@/public/images/team/supervision.png";
+import Admin from "@/public/images/team/admin.png";
+import D2 from "@/public/images/team/d2.png";
+
+export const Images = {
+    Discover,
+    Resources,
+    Verify,
+    Commit,
+    Commit2,
+    CurveU,
+    Curve,
+    CurveCyan,
+    Goat,
+    Student,
+    Teacher,
+    Lion,
+    ContactUs,
+    WhyChooseUsIllustration1,
+    WhyChooseUsIllustration2,
+    Info,
+    Hero,
+    HowItWork,
+    Davys,
+    Fnepi,
+    HomeFly,
+    Users,
+    TeacherIllustration,
+    WhatFamilySay,
+    WhatHelearn,
+    SeeEverything,
+    TrackAll,
+    HowItWorkHero,
+    HowItWorkIllustration,
+    HowItWorkOne,
+    HowItWorkTwo,
+    HowItWorkThree,
+    HowItWorkFour,
+    HowItWorkIllustration1,
+    HowItWorkIllustration2,
+    HowItWorkIllustration3,
+    HowItWorkIllustration4,
+    HowItWorkIllustration5,
+    HowItWorkIllustration6,
+    HowItWorkIllustration7,
+    HowItWorkIllustration8,
+    SelectionStepIllustration1,
+    SelectionStepIllustration2,
+    SelectionStepIllustration3,
+    SelectionStepIllustration4,
+    SelectionStepIllustration5,
+    SelectionStepIllustration6,
+    SelectionStepIllustration7,
+    SelectionStepIllustration8,
+    TeacherHorizScroll,
+    Teacher1,
+    Teacher2,
+    Teacher3,
+    Teacher4,
+    Teacher5,
+    Teacher6,
+    Teacher7,
+    Teacher8,
+    Avatar1,
+    Avatar2,
+    Avatar3,
+    Avatar4,
+    Avatar5,
+    Avatar6,
+    Avatar7,
+    Cta1,
+    Cta2,
+    Cta3,
+    Cta4,
+    Value1,
+    Value2,
+    Value3,
+    ChallengesLearning,
+    ChallengesBrain,
+    ChallengesBag,
+    ChallengesCup,
+    ChallengesHug,
+    ChallengesDirection,
+    ValuesExcellence,
+    ValuesProfessionnalisme,
+    ValuesInnovation,
+    ValuesIntegrity,
+    ValuesEngagement,
+    ValuesRespect,
+    Math,
+    PH,
+    SVT,
+    English,
+    French,
+    Spanish,
+    HG,
+    Philosophy,
+    Arabic,
+    Computer,
+    Pedagogie,
+    Supervision,
+    Admin,
+    D2,
+} as const;
