5153acfc7958789c5be17dd6deae3ebfad0997c8
ibraum <ibraumodnok@gmail.com>
2026-07-31 16:42:19 +0000

    updatre

--- src/components/ui/molecules/WhyFamilyCarousel.tsx
+++ src/components/ui/molecules/WhyFamilyCarousel.tsx
@@ -20,8 +20,8 @@ const WhyFamilyCarousel = ({ whyId = 1 }: WhyFamilyCarouselI) => {
             <div className='grid grid-rows-1 lg:grid-rows-[1fr_70px] gap-2'>
                 <div className={[currentWhyFamilyCarousel?.cardBackgroundColor, "rounded-lg order-0 flex flex-col items-start justify-between p-5"].join(" ")}>
                     <div className="flex flex-col items-start justify-start gap-2">
-                        <div className="max-lg:text-[18px]! max-lg:font-dm-serif title2">"{currentWhyFamilyCarousel?.title}"</div>
-                        <div className="max-lg:text-[14px]! max-lg:font-inter simpleText2">{currentWhyFamilyCarousel?.description}</div>
+                        <div className="max-lg:text-[18px]! max-lg:leading-[24px]! max-lg:font-dm-serif title2">"{currentWhyFamilyCarousel?.title}"</div>
+                        <div className="max-lg:text-[14px]! max-lg:leading-[20px]! max-lg:font-inter simpleText2">{currentWhyFamilyCarousel?.description}</div>
                     </div>
                     <Button className={[`${currentWhyFamilyCarousel?.buttonBackground} ${currentWhyFamilyCarousel?.buttonLabelColor}`, "max-lg:mt-6"].join(" ")}>{currentWhyFamilyCarousel?.buttonLabel}</Button>
                 </div>
